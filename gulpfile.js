1fe6d1ffff09c92f078d679faf15ba7a72c7ce88
Kostis Maninakis <kmaninak@gmail.com>
2017-09-18 20:00:04 +0300

    chore(gulp): use new dist folder to serve instead of src

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,47 @@
 'use strict';
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const browserSync = require('browser-sync');
+const gulp        = require('gulp'),
+      runSequence = require('run-sequence'),
+      del         = require('del'),
+      sass        = require('gulp-sass'),
+      browserSync = require('browser-sync');
 
 const reload = browserSync.reload;
 
+// default task executed with plain `gulp` command
+gulp.task('default', ['serve']);
 
-gulp.task('default', () => {
-  // place code for your default task here
+// delete `dist` folder
+gulp.task('clean', () => {
+  return del('dist');
 });
 
+// copy files from `src` to `dist` folder
+gulp.task('copy', () => {
+  return gulp.src([
+      'src/assets/**/*',
+      'src/*.html',
+      'src/js/**/*.js',
+    ])
+    .pipe(gulp.dest('dist'));
+});
+
+// compile scss files into css
 gulp.task('sass', () => {
   return gulp.src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('src/css'));
+    .pipe(gulp.dest('dist/css'));
 });
 
-// watch files for changes and reload if any detected
-gulp.task('serve', ['sass'], () => {
+// watch `src` folder for changes and reload browser if any are detected
+gulp.task('serve', runSequence('clean', ['copy', 'sass']), () => {
   browserSync({
-    server: {
-        baseDir: 'src',
-    },
+    server: { baseDir: 'dist' },
     open: false,
   });
 
   gulp.watch(
-    [
-      'src/*.html',
-      'src/sass/**/*.scss',
-      'src/js/**/*.js',
-    ],
-    ['sass', reload]
+    ['src/**/*'],
+    () => { runSequence('clean', ['copy', 'sass'], reload) }
   );
 });
