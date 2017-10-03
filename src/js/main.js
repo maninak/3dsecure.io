9cc03b0cc1a15d9630961fda4b0a8a6135aae3f6
Kostis Maninakis <kmaninak@gmail.com>
2017-10-03 18:57:29 +0300

    feat(): implement functionality to scroll page until an element comes into view

--- src/js/main.js
+++ src/js/main.js
@@ -47,6 +47,59 @@ function setPaddingBottomSlanted(el, desiredPadding, slantAngle) {
   el.style.paddingBottom = desiredPadding - (slantHeight / 2);
 }
 
+/**
+ * Scroll the page until the specified DOM element comes into view
+ * 
+ * Example:
+ * <a onclick="smoothScroll('info')">
+ * 
+ * @param {String} eID The id of the DOM element towards which to scroll the page
+ */
+function smoothScroll(eID) {
+  function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+  }
+
+  function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+  }
+
+  var startY = currentYPosition();
+  var stopY = elmYPosition(eID);
+  var distance = stopY > startY ? stopY - startY : startY - stopY;
+  if (distance < 100) {
+      scrollTo(0, stopY); return;
+  }
+  var speed = Math.round(distance / 100);
+  if (speed >= 20) speed = 20;
+  var step = Math.round(distance / 25);
+  var leapY = stopY > startY ? startY + step : startY - step;
+  var timer = 0;
+  if (stopY > startY) {
+      for ( var i=startY; i<stopY; i+=step ) {
+          setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+          leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+      } return;
+  }
+  for ( var i=startY; i>stopY; i-=step ) {
+      setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+      leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+  }
+}
+
 
 
 /*-----------------------------*\
